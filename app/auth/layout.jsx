9bf3aa2abe9e5bab8d2a06cb06a0dd5b6cb61838
Amadi Jerry <amadijerry310@gmail.com>
2023-12-03 09:39:38 +0100

    fixed form overflowing in signup page

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
-   <main className="bg-homeRadialBg4 h-screen">
+   <main className="bg-homeRadialBg4 h-full lg:h-screen">
     <Navbar />
     {children}
    </main>

f3fe31abf77c43ccb6156c42ceca47f1e2d94b77
Amadi Jerry <amadijerry310@gmail.com>
2023-12-03 09:58:28 +0100

    added extra bottom padding to signup page

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
-   <main className="bg-homeRadialBg4 h-full lg:h-screen">
+   <main className="bg-homeRadialBg4 mb-0 pb-[6rem] h-full lg:h-screen">
     <Navbar />
     {children}
    </main>

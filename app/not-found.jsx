8c8babbaff1db3db671c47cbfafd406964cdabca
Amadi Jerry <amadijerry310@gmail.com>
2023-12-04 04:09:08 +0100

    removed hyphen in notFound image file name

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import notFound from "./not-found.png";
+import notFound from "./notFound.png";
 import Navbar from "./components/Navbar";
 import Link from "next/link";
 

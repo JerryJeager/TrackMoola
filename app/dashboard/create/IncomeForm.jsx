2a72622c9623ee135a8a51749c0d1e87d445764f
Amadi Jerry <amadijerry310@gmail.com>
2023-12-08 06:37:45 +0100

    created incomeForm

--- app/dashboard/create/IncomeForm.jsx
+++ app/dashboard/create/IncomeForm.jsx
@@ -1,9 +1,64 @@
-
+import { transactionDateFormat } from "@/app/lib/helpers/dateFormat";
+import { useState } from "react";
 
 const IncomeForm = () => {
+  const [date, setDate] = useState(transactionDateFormat());
+  const [amount, setAmount] = useState();
+  const [description, setDescription] = useState();
+  const [wallet, setWallet] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>IncomeForm</div>
-  )
-}
+    <form
+      onSubmit={handleSubmit}
+      className="text-white  mt-4 p-2 lg:mt-8 lg:p-4"
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <label className="transaction-form-label">
+          <span>Transaction</span>
+          <input
+            className="w-full"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <label className="transaction-form-label">
+          <span>Wallet</span>
+          <select
+            value={wallet}
+            onChange={(e) => setWallet(e.target.value)}
+            name=""
+            id=""
+            className="bg-lightBlue"
+          >
+            <option value="Wallet 1">Wallet 1</option>
+            <option value="Wallet 2">Wallet 2</option>
+          </select>
+        </label>
+        <label className="transaction-form-label">
+          <span>Amount</span>
+          <input
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            type="number"
+          />
+        </label>
+        <label className="transaction-form-label">
+          <span>Description</span>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="resize-none h-[50px]"
+          ></textarea>
+        </label>
+      </div>
+      <button className="p-2 bg-secondary text-white mt-4 lg:mt-8 rounded-md">
+        Add Income
+      </button>
+    </form>
+  );
+};
 
-export default IncomeForm
+export default IncomeForm;

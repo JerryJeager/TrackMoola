5a4347b911b6d5850b150c9c323abd84d81a270e
Amadi Jerry <amadijerry310@gmail.com>
2024-01-07 19:43:55 +0100

    feat: enabled creating of expenses and updating amount on budget category

--- app/dashboard/create/ExpensesForm.tsx
+++ app/dashboard/create/ExpensesForm.tsx
@@ -1,18 +1,105 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { transactionDateFormat } from "../../lib/helpers/dateFormat";
+import { useAuthContext } from "../../context/AuthContext";
+import supabase from "../../lib/supabase/server";
+import toast, { Toaster } from "react-hot-toast";
 
 const ExpensesForm = () => {
+  const {user} = useAuthContext()
   const [date, setDate] = useState(transactionDateFormat());
   const [amount, setAmount] = useState(null);
   const [expenseName, setExpenseName] = useState('');
   const [wallet, setWallet] = useState('')
   const [description, setDescription] = useState('');
-  const [budgetCategory, setBudgetCategory] = useState('');
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [budgetCategory, setBudgetCategory] = useState(0);
+  const [userWallets, setUserWallets] = useState([])
+  const [userBudgets, setUserBudgets] = useState([])
+
+  const notify = (content: string) => toast(content)
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try{
+      // console.log(budgetCategory)
+      console.log(userBudgets)
+      e.preventDefault()
+      let {error, data} = await  supabase
+      .from("expenses")
+      .insert({created_at: date, expense_name: expenseName, amount: amount, description: description, category_id: Number(budgetCategory)})
+
+      if(error) throw new Error(error.message)
+      if(data) console.log(data)
+
+      updateBudgetAmount(amount)
+    }
+    catch(error){
+      console.log(error)
+    }
   };
+
+  const getWallets = async () => {
+    try {
+      const { error, data } = await supabase
+        .from("wallets")
+        .select()
+        .eq("user_id", user.id);
+
+      console.log(data);
+      setUserWallets([...data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getBudgets = async (id: string) => {
+    try{
+      // console.log(wallet)
+      // console.log(id)
+      
+      const {error, data} = await supabase
+      .from("categories")
+      .select()
+      .eq("wallet_id", id )
+      
+      if(error) throw new Error(error.message)
+      if(data) setUserBudgets(data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const updateBudgetAmount = async (exp: number) => {
+    try{
+      let currentAmount = userBudgets.filter(budget => budget.id === Number(budgetCategory))
+      // console.log(typeof budgetCategory)
+      console.log(currentAmount)
+      
+      const {error, data} = await supabase
+      .from("categories")
+      .update({amount: currentAmount[0].amount - exp})
+      .eq("id", budgetCategory)
+      .select()
+
+      if(data) console.log(data)
+      if(error) throw new Error(error.message)
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  // const updateMainAccountAmount = async ()
+
+  useEffect(() => {
+    getWallets()
+  }, [])
+
+  useEffect(() => {
+    if(userBudgets.length < 1 && wallet !== "") notify("You've not created any budget category for this wallet")
+  }, [userBudgets])
   return (
+    <>
     <form
       onSubmit={handleSubmit}
       className="text-white  mt-4 p-2 lg:mt-8 lg:p-4"
@@ -31,29 +118,40 @@ const ExpensesForm = () => {
           <span>Wallet</span>
           <select
             value={wallet}
-            onChange={(e) => setWallet(e.target.value)}
+            onChange={(e) => {
+              setWallet(e.target.value)
+              getBudgets(e.target.value)
+              }}
             name=""
             id=""
+            required
             className="bg-lightBlue"
           >
-            <option value="Wallet 1">Wallet 1</option>
-            <option value="Wallet 2">Wallet 2</option>
+          <option value="" hidden>Select a Wallet</option>
+            {
+              userWallets && userWallets.map(w => 
+              (
+                <option value={w.id}>{w.wallet_name}</option>
+              ))
+            }
           </select>
         </label>
         <label className="transaction-form-label">
           <span>Budget Category</span>
           <select
             value={budgetCategory}
-            onChange={(e) => setBudgetCategory(e.target.value)}
+            onChange={(e) => setBudgetCategory(Number(e.target.value))}
             name=""
             id=""
             className="bg-lightBlue"
           >
-            <option value="Groceries">Groceries</option>
-            <option value="Health">Health</option>
-            <option value="Apparels">Apparels</option>
-            <option value="Electronics">Electronics</option>
-            <option value="LIfe">Life</option>
+          {userBudgets.length < 1 && <option value="" hidden>No available budget category</option>}
+            {
+              userBudgets && userBudgets.map(b => 
+              (
+                <option value={b.id}>{b.category_name}</option>
+              ))
+            }
           </select>
         </label>
         <label className="transaction-form-label">
@@ -86,6 +184,9 @@ const ExpensesForm = () => {
         Add Expense
       </button>
     </form>
+
+    <Toaster />
+    </>
   );
 };
 
